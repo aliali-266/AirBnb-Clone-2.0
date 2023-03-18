9532b1301975739b1078ca0d68b1ea342086c960
Ali Ali <aliali_266@yahoo.com>
2023-03-18 12:07:46 -0700

    Built left side of Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,35 @@
-import React from 'react'
+// import React from 'react'
+import Image from "next/image"
 
 function Header() {
   return (
-    <div>
-        <h1>I am the header</h1>
-    </div>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+
+        {/* LEFT */}
+        <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <Image
+
+            src= "https://links.papareact.com/qd3"
+            fill
+            objectFit="contain"
+            objectPosition="left"
+
+            />
+        </div>
+
+        {/* MIDDLE */}
+        <div>
+
+        </div>
+
+        {/* RIGHT */}
+        <div>
+
+        </div>
+
+
+
+    </header>
   )
 }
 
