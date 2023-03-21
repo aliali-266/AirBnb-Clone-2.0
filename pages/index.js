583d98301df05b8d02c74d3d3175b85b6b4c145f
Ali Ali <aliali_266@yahoo.com>
2023-03-21 14:33:09 -0700

    Finished the small card features

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import SmallCard from "@/components/SmallCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -22,18 +23,34 @@ export default function Home() {
       <Banner />
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
-
-
         <section className="pt-6">
-
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
+          {/* Pull data from server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
         </section>
-
-
       </main>
-
-
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch(
+    "https://api.npoint.io/c7c0a1055eaba72194a3"
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
